ca85625a94b90e41461762943e55b0d812414f1b
Erick Daniel <erickdanielalveslima@gmail.com>
2024-11-19 23:34:59 -0300

    feat: created post route by id

--- server.js
+++ server.js
@@ -2,26 +2,32 @@ import express from 'express';
 
 const posts = [
     {
+        id: 1,
         descricao: "uma foto teste",
         imagem: "https://placecats.com/millie/300/150"
     },
     {
+        id: 2,
         descricao: "Gato fazendo yoga",
         imagem: "https://placecats.com/millie/300/150"
     },
     {
+        id: 3,
         descricao: "Paisagem montanhosa",
         imagem: "https://placecats.com/millie/300/150"
     },
     {
+        id: 4,
         descricao: "Cachorro sorrindo",
         imagem: "https://placecats.com/millie/300/150"
     },
     {
+        id: 5,
         descricao: "Comida deliciosa",
         imagem: "https://placecats.com/millie/300/150"
     },
     {
+        id: 6,
         descricao: "Citação inspiradora",
         imagem: "https://placecats.com/millie/300/150"
     }
@@ -44,4 +50,17 @@ app.get('/api', (req, res) => {
 
 app.get('/api/posts', (req, res) => {
     res.status(200).json(posts);
+});
+
+
+function getPostById(id) {
+    return posts.findIndex((post) => {
+        return post.id === Number(id);
+    });
+};
+
+
+app.get('/api/posts/:id', (req, res) => {
+    const index = getPostById(req.params.id);
+    res.status(200).json(posts[index]);
 });
